c2764bd5d12b2a0ecb77003fbaf3e198bcc5c9e6
Byyyrd <105438850+Byyyrd@users.noreply.github.com>
2024-02-15 18:25:36 +0100

    Iter and Rec

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(fibInterativ(8));
+        System.out.println(factRec(3));
     }
 
-    public static int fibInterativ(int n) {
-        if (n == 2)
-            return 2;
-        if (n <= 1)
-            return 0;
+    public static int fibIter(int n) {
+        if (n <= 2)
+            return 1;
 
         int first = 1;
         int second = 1;
@@ -23,5 +21,27 @@ public class Main {
 
         return third;
 
+    }
+
+    public static int fibRec(int n) {
+        if(n <= 2)
+            return 1;
+
+        return fibRec(n-1) + fibRec(n-2);
+
+    }
+
+    public static int factRec(int n) {
+        if(n == 0)
+            return 1;
+        return n * factRec(n-1);
+    }
+
+    public static int factIter(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
     }
 }
